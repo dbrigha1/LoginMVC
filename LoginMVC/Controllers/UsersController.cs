728d6bd16211d1ad8b81e8d61999b80df5b02c76
Dylan A Brigham <dbrigha1@uwyo.edu>
2018-03-04 11:48:43 -0700

    Add project files.

---
+++ LoginMVC/Controllers/UsersController.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using LoginMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace LoginMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+       // private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        
+
+        public UsersController()
+        {
+        }
+
+        public UsersController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(UserManager.Users.ToList());
+        }
+
+        //// GET: Users/Details/5
+        //public ActionResult Details(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ApplicationUser applicationUser = db.Users.Find(id);
+        //    if (applicationUser == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(applicationUser);
+        //}
+
+        //// GET: Users/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Users/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,UserRole,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Users.Add(applicationUser);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(applicationUser);
+        //}
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = UserManager.FindById(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task <ActionResult> Edit([Bind(Include = "Id,UserRole,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get the existing student from the db
+                var user = await UserManager.FindByIdAsync(applicationUser.Id);
+                
+               // PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName
+                // Update it with the values from the view model
+               // user.Id = applicationUser.Id;
+              //  user.UserRole = applicationUser.UserRole;
+                user.Email = applicationUser.Email;
+                user.EmailConfirmed = applicationUser.EmailConfirmed;
+                user.PasswordHash = applicationUser.PasswordHash;
+                user.SecurityStamp = applicationUser.SecurityStamp;
+                user.PhoneNumber = applicationUser.PhoneNumber;
+                user.PhoneNumberConfirmed = applicationUser.PhoneNumberConfirmed;
+                user.TwoFactorEnabled = applicationUser.TwoFactorEnabled;
+                user.LockoutEndDateUtc = applicationUser.LockoutEndDateUtc;
+                user.LockoutEnabled = applicationUser.LockoutEnabled;
+                user.AccessFailedCount = applicationUser.AccessFailedCount;
+                user.UserName = applicationUser.UserName;
+
+                await UserManager.RemoveFromRoleAsync(user.Id, user.UserRole);
+                user.UserRole = applicationUser.UserRole;
+                await UserManager.AddToRoleAsync(user.Id, user.UserRole);  
+                
+                var result = await UserManager.UpdateAsync(user);
+                //db.Entry(applicationUser).State = EntityState.Modified;
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(applicationUser);
+        }
+
+        //// GET: Users/Delete/5
+        //public ActionResult Delete(string id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    ApplicationUser applicationUser = db.Users.Find(id);
+        //    if (applicationUser == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(applicationUser);
+        //}
+
+        //// POST: Users/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(string id)
+        //{
+        //    ApplicationUser applicationUser = db.Users.Find(id);
+        //    db.Users.Remove(applicationUser);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
---
+++ LoginMVC/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin;
+using Owin;
+using LoginMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartupAttribute(typeof(LoginMVC.Startup))]
+namespace LoginMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRole();
+        }
+
+        public void CreateRole()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Administrator";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Guest"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Guest";
+                roleManager.Create(role);
+            }
+        }
+    }
+}
